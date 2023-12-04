0e10d52d50a152d65114a92833d8e0fc4a186ce3
michele <michele.giovagnoli1@gmail.com>
2023-12-04 12:48:19 +0100

    Modifica michele
    Modifica numero 3

--- AppTest/src/App.java
+++ AppTest/src/App.java
@@ -2,9 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println(10 + 23);
-        System.out.println("Modifiche di Michele");
-        System.out.println("Modifiche di Michele 2");
+        System.out.println("Modifica di Michele 3");
         System.out.println("Manzo scusami tanto non sei una merda :( :( :( !!!");
-       
+
     }
 }
