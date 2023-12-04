fdf2d3b0a7083ec34529eff4a113418cb883d3f8
michele <michele.giovagnoli1@gmail.com>
2023-12-04 11:55:28 +0100

    nuovo branch
    Nuovo branch con modifiche

--- AppTest/src/App.java
+++ AppTest/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println(10 + 23);
+        System.out.println("Modifiche di Michele");
     }
 }
